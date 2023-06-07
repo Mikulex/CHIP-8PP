d21923b2737f851b7c34a160dc00db12825125ed
Mikulex <mikulex@mikulex.com>
2023-06-08 00:42:17 +0200

    add sfml library for drawing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,58 @@
 #include "CPU.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-void drawScreen(CPU& cpu){
-    std::cout << "drawing screen" << std::endl;
+
+void drawScreen(CPU& cpu, sf::RenderWindow& window, sf::Image& image) {
     for (int y = 0; y < cpu.screen.size(); y++) {
-        std::cout << "ROW NUMBER " << y << '\t';
         for (int x = 0; x < cpu.screen[y].size(); x++) {
             if (cpu.screen[y][x]) {
-                std::cout << '#';
-            } else {
-                std::cout << ' ';
+                image.setPixel(x,y,sf::Color::Green);
             }
-        }
-        std::cout << std::endl;
+            else {
+                image.setPixel(x, y, sf::Color::Black);
+                
+            }
+        } 
     }
-
 }
 
 int main() {
+    int scale = 8;
+    sf::RenderWindow window(sf::VideoMode(64 * scale, 32 * scale), "CHIP-8PP Emulator");
     CPU cpu{};
     cpu.loadRam();
 
-    long long i = 0;
-    while (true) {
+    window.setFramerateLimit(60);
+    sf::Image image;
+    image.create(64 * scale, 32 * scale);
+    sf::Texture texture;
+    texture.loadFromImage(image);
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.setScale(sf::Vector2f(scale, scale));
+
+    while (window.isOpen())
+    {
         cpu.execute();
-        
-        if (i == 50000000) {
-            drawScreen(cpu);
-            i = 0;
+
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
-        i++;
+        
+        window.clear(sf::Color::Black);
+        drawScreen(cpu, window, image);
+        texture.update(image);
+        
+        window.draw(sprite);
+
+        window.display();
+
     }
 
     return 0;
